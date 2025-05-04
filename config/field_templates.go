a6191c6a09be4d231df90d10f4d0dbaaff0bc681
janmarkuslanger <janmarkuslanger10121994@gmail.com>
2025-05-04 13:32:22 +0200

    implement textarea

--- config/field_templates.go
+++ config/field_templates.go
@@ -1,8 +1,9 @@
 package config
 
 var FieldTemplates = map[string]string{
-	"Text":    "template_fields/text",
-	"Number":  "template_fields/number",
-	"Boolean": "template_fields/boolean",
-	"Date":    "template_fields/date",
+	"Text":     "template_fields/text",
+	"Number":   "template_fields/number",
+	"Boolean":  "template_fields/boolean",
+	"Date":     "template_fields/date",
+	"Textarea": "template_fields/textarea",
 }
