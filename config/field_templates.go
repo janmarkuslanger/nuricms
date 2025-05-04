a31e9b3320b0722e4abcd608d26a0c1023b1d9ca
janmarkuslanger <janmarkuslanger10121994@gmail.com>
2025-05-04 13:16:42 +0200

    implement date

--- config/field_templates.go
+++ config/field_templates.go
@@ -4,4 +4,5 @@ var FieldTemplates = map[string]string{
 	"Text":    "template_fields/text",
 	"Number":  "template_fields/number",
 	"Boolean": "template_fields/boolean",
+	"Date":    "template_fields/date",
 }
