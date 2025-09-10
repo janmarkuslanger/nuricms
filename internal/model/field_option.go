b74fc8f05679c4f998130bfb7236eeebeb873d83
janmarkuslanger <janmarkuslanger10121994@gmail.com>
2025-09-10 21:27:30 +0200

    add contraint for deleting Field -> FieldOption

--- internal/model/field_option.go
+++ internal/model/field_option.go
@@ -7,14 +7,14 @@ import (
 type FieldOptionType string
 
 const (
-	FieldOptionTypeSelectOption FieldType = "SelectOption"
+	FieldOptionTypeSelectOption FieldOptionType = "SelectOption"
 )
 
 type FieldOption struct {
 	gorm.Model
 	OptionType FieldOptionType `gorm:"type:varchar(20);not null"`
 	FieldID    uint            `gorm:"not null"`
-	Field      Field           `gorm:"foreignKey:FieldID"`
+	Field      Field           `gorm:"foreignKey:FieldID;constraint:OnDelete:CASCADE"`
 
 	Value string `gorm:"type:text"`
 }
