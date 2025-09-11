4bd00bf16723b120d381b2f5f21a4f5914e860e8
janmarkuslanger <janmarkuslanger10121994@gmail.com>
2025-09-11 22:14:54 +0200

    set FieldOption Value as not null

--- internal/model/field_option.go
+++ internal/model/field_option.go
@@ -16,5 +16,5 @@ type FieldOption struct {
 	FieldID    uint            `gorm:"not null"`
 	Field      Field           `gorm:"foreignKey:FieldID;constraint:OnDelete:CASCADE"`
 
-	Value string `gorm:"type:text"`
+	Value string `gorm:"type:text;not null"`
 }
